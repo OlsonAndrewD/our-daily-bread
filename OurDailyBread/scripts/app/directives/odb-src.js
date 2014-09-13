cc7bdc3d0cb7a7b6ce53d61e8ef0dcbe00ab007a
Andrew <olson.andrew.d@gmail.com>
2014-09-13 16:12:18 -0500

    Audio works.

---
+++ OurDailyBread/scripts/app/directives/odb-src.js
@@ -0,0 +1,14 @@
+﻿angular.module("odb").directive("odbSrc", [
+    function () {
+        return {
+            scope: {
+                url: "="
+            },
+            link: function (scope, elem) {
+                scope.$watch("url", function (newUrl) {
+                    elem.attr("src", newUrl);
+                });
+            }
+        };
+    }
+]);
--- OurDailyBread/scripts/app/todayController.js
+++ OurDailyBread/scripts/app/todayController.js
@@ -1,7 +1,25 @@
 ﻿angular.module("odb").controller("TodayController", [
-    "$scope",
-    function ($scope) {
+    "$scope", "$http",
+    function ($scope, $http) {
+        var apiKey = "dcf32e6c4f77d824036c627c94090613";
+        var queryParams = "key=" + apiKey + "&v=2";
+        var audioRootUrl, videoRootUrl;
+
         $scope.scriptureReference = "Genesis 1";
         $scope.contentType = "audio";
+
+        $http.get("http://dbt.io/audio/location?" + queryParams).
+            success(function (data) {
+                var locationInfo = data[0];
+                audioRootUrl = locationInfo.protocol + "://" + locationInfo.server + locationInfo.root_path + "/";
+                updateAudioSource();
+            });
+
+        function updateAudioSource() {
+            $http.get("http://dbt.io/audio/path?" + queryParams + "&dam_id=ENGESVO2DA&book_id=Ps&chapter_id=90").
+                success(function (data) {
+                    $scope.audioSourceUrl = audioRootUrl + data[0].path;
+                });
+        };
     }
 ]);
